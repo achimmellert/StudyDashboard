6e74b35d2f24983e3396f578b797f86a83346a20
achimmellert <122669576+achimmellert@users.noreply.github.com>
2025-03-28 10:13:42 +0100

    Update DataHandler_ToDos_LearnStreak.py
    Vollständige Überarbeitung

--- DataHandler_ToDos_LearnStreak.py
+++ DataHandler_ToDos_LearnStreak.py
@@ -1,29 +1,20 @@
-# DataHandler_Studium.py
-import json
+# DataHandler_ToDos_LernStreak.py
 from dataclasses import dataclass, asdict
-from typing import List, Dict
 import os
-from file_manager import JSONFileManager
+from typing import List
+from file_manager import JSONFileManager, TextFileManager
 
-# Basisverzeichnis und Dateipfade
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 JSON_FILE_DIRECTORY = os.path.join(BASE_DIR, 'data_files')
 os.makedirs(JSON_FILE_DIRECTORY, exist_ok=True)
-
-JSON_FILE_NAME = 'studium.json'
+JSON_FILE_NAME = 'todos.json'
 JSON_FILE_ABS_PATH = os.path.join(JSON_FILE_DIRECTORY, JSON_FILE_NAME)
-
-GRADE_JSON_PATH = os.path.join(JSON_FILE_DIRECTORY, 'grades.json')
 
 
 @dataclass
-class Module:
-    name: str = ""
-    professor: str = ""
-    topics: str = ""
-    notes: str = ""
-    progress: int = 0  # Fortschritt in Prozent (0–100)
-    project: str | None = None
-    exam_date: str | None = None
-    exam_grade: float = 0.0
+class ToDo:
+    description: str
+    module: str
+    time: str
+    is_done: bool
     
